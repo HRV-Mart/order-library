6c088ba91475e9bbc1db7ff1c4ef0dc90a3a5fb6
Harsh3305 <harshverma3305@gmail.com>
2023-04-05 14:25:19 +0530

    Change build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/hrv-mart/user-library")
+            url = uri("https://maven.pkg.github.com/hrv-mart/order-library")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
